28d688e174264fe8816e18b859ea8a5f8102495e
shadowstiles <successforemmy@gmail.com>
2022-10-14 06:29:31 -0700

    small letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,7 +5,17 @@
  */
 int main(void)
 {
-	char alpha[27] = 'abcdefghijklmnopqrstuvwxyz';
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz"
 
-	putchar(alpha);
+	for (int count = 0; count < 28; count++)
+	{
+		if (alpha[count] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(alpha[count]);
+	}
+	return (0);
 }
