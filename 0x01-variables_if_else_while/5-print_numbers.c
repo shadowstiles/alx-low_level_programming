66cac32e8ae8a3e7140857b106f18aae04651de1
shadowstiles <successforemmy@gmail.com>
2022-10-14 19:54:40 -0700

    5 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,19 +5,17 @@
  */
 int main(void)
 {
-  int count = 0;
+	int count = 0;
 
-  while (count < 11) 
-  {
-    if (count == 10) 
-    {
-      printf("\n");
-      break;
-    } else
-    {
-      printf("%i", count);
-      count++;
-    }
-  }
-  return (0);
+	while (count < 11)
+	{
+		if (count == 10)
+		{
+			printf("\n");
+			break;
+		}
+		printf("%i", count);
+		count++;
+	}
+	return (0);
 }
