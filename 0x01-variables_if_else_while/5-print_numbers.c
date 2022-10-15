a73038305a0fe20f0460adcf6de428b7f632badc
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-10-15 03:02:53 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * * main - Entry
- * * Return: 0
- * */
-int main(void) {
-	    int count = 0;
-
-	        while (count < 11) {
-			        if (count == 10) {
-					            printf("\n");
-						                break;
-								        }
-				        else
-						        {
-								            printf("%i", count);
-									                count++;
-											        }
-					    }
-		    return (0);
-=======
  * main - Entry
  * Return: 0
  */
-int main(void) {
+int main(void) 
+{
     int count = 0;
 
     while (count < 11) {
@@ -38,5 +19,4 @@ int main(void) {
         }
     }
     return (0);
->>>>>>> c6ed6da485a3fe6c055d9f128b98b2d74caff76e
 }
