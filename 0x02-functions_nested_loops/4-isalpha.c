be888f2b320f9577d052d47772e7a73502d6ee05
shadowstiles <successforemmy@gmail.com>
2022-10-18 16:18:59 -0700

    both case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - uppercase, letter etv
+ * @c: imput
+ * Returns: value
+ */
+int _isalpha(int c)
+{
+	int value;
+
+	if (isalpha(c))
+	{
+		value = 1;
+	}
+	else
+	{
+		value = 0;
+	}
+	return (value);
+}
