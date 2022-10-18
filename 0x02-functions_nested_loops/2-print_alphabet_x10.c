839da332daabee69964fb6187ac67865ffef3abc
shadowstiles <successforemmy@gmail.com>
2022-10-18 07:36:01 -0700

    x10 alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 /**
- * main -Entry
- * Return: 0
- */
-void print_alphabet_x10(void)
-{
-	char alphabet_lower[] = "abcdefghijklmnopqrstuvwxyz";
-	int count = 0;
-	int limit = 0;
+ * * main -Entry
+ * * Return: 0
+ * */
+void print_alphabet_x10(void) {
+	    char alphabet_lower[] = "abcdefghijklmnopqrstuvwxyz";
+	        int count = 0;
+		    int limit = 0;
 
-	while (limit < 10)
-	{
-		while (count < 28)
-		{
-			if (alphabet_lower[count] == '\0')
-			{
-				putchar('\n');
-				break;
-			}
-			putchar(alphabet_lower[count]);
-			count++;
-		}
-		limit++;
-	}
+		        while (limit < 10) {
+				        while (count < 28) {
+						            if (alphabet_lower[count] == '\0') {
+								                    putchar('\n');
+										                    break;
+												                }
+							                putchar(alphabet_lower[count]);
+									            count++;
+										            }
+					        limit++;
+						        count = 0;
+							    }
 }
 
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
+int main(void) {
+	    print_alphabet_x10();
+	        return (0);
 }
