dd0a273c40011969c831ad19eb8664ee0ef3d1db
shadowstiles <successforemmy@gmail.com>
2022-10-19 15:33:07 -0700

    finobocci number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,17 +7,18 @@
 int main(void)
 {
 	int a = 1;
-	long long int  b = 2;
+	long int  b = 2;
 	int count = 2;
 	int c;
 
 	printf("%i, ", a);
+	printf("%ld, ", b);
 	while (count < 51)
 	{
 		c = b;
 		b = a + b;
 		a = c;
-		printf("%lld", b);
+		printf("%ld", b);
 		if (count < 49)
 		{
 			putchar(',');
