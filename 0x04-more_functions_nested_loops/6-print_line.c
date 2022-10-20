b85fb907b1be95d2d22dbb67b033e66d808aa97f
shadowstiles <successforemmy@gmail.com>
2022-10-20 09:32:22 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print straight lines
+ * @n: given number
+ * Return: line
+ */
+
+void print_line(int n)
+{
+	int count = 1;
+
+	if (n == 0)
+		_putchar('\n');
+
+	while (count <= n)
+	{
+		_putchar('_');
+		_putchar('\n');
+		count++;
+	}
+}
