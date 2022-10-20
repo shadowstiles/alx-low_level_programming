78c8776e6b480d093d28ac81e54f004bc33c0fde
shadowstiles <successforemmy@gmail.com>
2022-10-20 10:10:12 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print straight lines
+ * @n: given number
+ * Return: line
+ */
+
+void print_diagonal(int n)
+{
+	int count = 1;
+	int space = 1
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (count <= n)
+	{
+		while (space < count)
+		{
+			_putchar(' ');
+			space++;
+		}
+
+		space = 1,
+		_putchar('\');
+		_putchar('\n');
+
+		count++;
+	}
+}
