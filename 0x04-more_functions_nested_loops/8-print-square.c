9c6e511afdc01b82862bddae1c63c9edc7dc1d86
shadowstiles <successforemmy@gmail.com>
2022-10-20 10:37:00 -0700

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print-square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: given length
+ * Return: line
+ */
+
+void print_diagonal(int size)
+{
+	int count = 1;
+	int hash = 1;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (count <= size)
+	{
+		while (hash <= size)
+		{
+			_putchar('#');
+			hash++;
+		}
+
+		hash = 1,
+		_putchar('\n');
+
+		count++;
+	}
+}
