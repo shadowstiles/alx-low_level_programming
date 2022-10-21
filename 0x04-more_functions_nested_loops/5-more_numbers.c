54545f25df1bb679ca5b4afbd30497baaeba74db
shadowstiles <successforemmy@gmail.com>
2022-10-20 17:38:03 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,15 @@ void more_numbers(void)
 {
 	int i, a;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (a = 0; a < 15; a++)
+		for (a = 0; a < 14; a++)
 		{
-			printf("%i", a);
+			if (a > 9)
+			{
+				_putchar((a / 10) + '0');
+			}
+			_putchar((a % 10) + '0');
 		}
 
 		_putchar('\n');
