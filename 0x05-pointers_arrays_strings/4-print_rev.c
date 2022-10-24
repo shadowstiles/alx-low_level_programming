08cd9cd0c5c0695303c12678bb0789a173874346
shadowstiles <successforemmy@gmail.com>
2022-10-24 10:51:06 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - writes to th standard output
+ * @s: character to reverse
+ * Return: 0 OR 1
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
