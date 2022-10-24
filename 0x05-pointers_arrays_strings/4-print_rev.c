4311cde2921c28553c0152b60a5282cc81cc717d
shadowstiles <successforemmy@gmail.com>
2022-10-24 15:36:21 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,29 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
  * print_rev - writes to th standard output
  * @s: character to reverse
  * Return: 0 OR 1
  */
-char *strrev(char *str)
-{
-	char *p1, *p2;
-	
-	if (! str || ! *str)
-		return str;
-	for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
-	{
-		*p1 ^= *p2;
-		*p2 ^= *p1;
-		*p1 ^= *p2;
-	}
-	return str;
-}
 
 void print_rev(char *s)
 {
-	char h = strrev(s);
-	printf("%s\n", h);
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
 }
