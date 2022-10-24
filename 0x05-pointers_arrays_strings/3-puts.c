f8d819f196408a6ce1e004b328bbfc487de1d6b1
shadowstiles <successforemmy@gmail.com>
2022-10-24 10:44:47 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - writes to th standard output
+ * @str: character to print
+ * Return: 0 OR 1
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
