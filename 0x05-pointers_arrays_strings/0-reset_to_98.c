5debd1c6bbf8aef5e738a3146ec042f2be49eee9
shadowstiles <successforemmy@gmail.com>
2022-10-24 08:32:07 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes every value in memory to point to 98
+ * @n: value to change
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints to the standared output
+ * @c: inputed value
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+	return (write(1, %c, 1));
+}
