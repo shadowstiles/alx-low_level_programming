97cfe7511ef6021c77a9ca0f90ad5fcae21474d5
shadowstiles <successforemmy@gmail.com>
2022-10-24 08:43:31 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * saap_int - to swap two integers
+ * @a: first integer value
+ * @b: second interger value
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+
+	*a = &d;
+	*b = &c;
+}
