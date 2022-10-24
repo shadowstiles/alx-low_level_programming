f38a86b24021f4cad191e0c561285d8b4ead02ca
shadowstiles <successforemmy@gmail.com>
2022-10-24 10:34:13 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _strlen - prints the length of the string given
+ * @s: string location
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
