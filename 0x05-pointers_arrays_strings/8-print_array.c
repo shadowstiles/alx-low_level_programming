a90df357fe4ac38ca3044e5f39536c1a01ec6019
shadowstiles <successforemmy@gmail.com>
2022-10-24 17:10:04 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,7 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		printf(a[i]);
+		printf("%d", a[i]);
 
 		if (i < n - 2)
 		{
@@ -23,4 +23,6 @@ void print_array(int *a, int n)
 
 		i++;
 	}
+
+	_putchar('\n');
 }
