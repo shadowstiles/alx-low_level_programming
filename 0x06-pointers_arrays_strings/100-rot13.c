47992449523da474eee9db031b4f379d50502a81
shadowstiles <successforemmy@gmail.com>
2022-10-28 18:42:35 -0700

    rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,6 +9,7 @@
 char *rot13(char *c)
 {
 	char lower[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char code[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM"
 	int i1 = 0;
 	int i2;
 
@@ -16,14 +17,9 @@ char *rot13(char *c)
 	{
 		for (i2 = 0; lower[i2] != '\0'; i2++)
 		{
-			if (c[i1] == lower[i2] && i2 < 26)
+			if (c[i1] == lower[i2])
 			{
-				c[i1] = lower[i2 + 26];
-				break;
-			}
-			else
-			{
-				c[i1] = lower[26 - i2];
+				c[i1] = code[i2];
 				break;
 			}
 		}
