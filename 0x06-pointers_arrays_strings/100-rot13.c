a7faf87a9ac8a581d000196338b0f47b0ef2146d
shadowstiles <successforemmy@gmail.com>
2022-10-28 18:09:56 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * rot13 - capitalize string
- * @c: string to convert
+ * rot13 - encodes string in rot13
+ * @c: string encode
  * Return: result
  */
 
 char *rot13(char *c)
 {
-	char lower[] = "abcdefghijklmnopqrstuvwxyz";
-	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char lower[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
 	int i1 = 0;
 	int i2;
 
@@ -17,14 +16,14 @@ char *rot13(char *c)
 	{
 		for (i2 = 0; lower[i2] != '\0'; i2++)
 		{
-			if ((c[i1] == lower[i2] || c[i1] == upper[i2]) && i2 <= 13)
+			if (c[i1] == lower[i2] && i2 < 26)
 			{
-				c[i1] = code[i2 + 13];
+				c[i1] = lower[i2 + 26];
 				break;
 			}
 			else
 			{
-				c[i1] = code[13 - i2];
+				c[i1] = lower[26 - i];
 				break;
 			}
 		}
