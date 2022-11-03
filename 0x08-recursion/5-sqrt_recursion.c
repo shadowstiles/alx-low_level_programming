7e49a3dfbd4bed80625c3b99da860054bf4fb90d
shadowstiles <successforemmy@gmail.com>
2022-11-03 04:45:26 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,5 +9,23 @@
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n));
+	int c, ans;
+
+	c = count();
+	ans = c * c;
+
+	if (ans != n && n == c)
+		return (-1);
+
+	if (ans == n)
+		return (ans);
+
+	return (_sqrt_recursion(n));
+}
+
+int count(void)
+{
+	int i = 1;
+
+	return (i + 1);
 }
