c82b0eee167e2b2911124e0111f60e103f11ad1d
shadowstiles <successforemmy@gmail.com>
2022-11-04 09:18:47 -0700

    whats is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argc
+ * argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+
+	printf("\n");
+
+	return (0);
+}
