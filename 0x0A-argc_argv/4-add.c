570393646336bc5d31c553dc79b0eaf340b37b22
shadowstiles <successforemmy@gmail.com>
2022-11-04 15:26:37 -0700

    add 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int num;
+	int ans;
+
+	if (argc == 0)
+		printf("%d", 0);
+
+	for (i = 1; i < argc; i++)
+	{
+		num = strtol(argv[i], NULL, 10);
+
+		if (num == 0)
+		{
+			printf("Error\n");
+
+			return (1);
+		}
+		else
+			ans = ans + num;
+	}
+
+	printf("%d\n", ans);
+
+	return (0);
+}
