76a4ef55b6198dcc18c0c8c0a33b39a2adbc7035
shadowstiles <successforemmy@gmail.com>
2022-11-08 15:10:11 -0800

    String concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	int size2 = sizeof(s2);
 	int size = sizeof(s1) + sizeof(s2);
 
-	ar = malloc((size - 2) * sizeof(char));
+	ar = malloc((size - sizeof(char)) * sizeof(char));
 
 	if (ar == NULL)
 		return (NULL);
