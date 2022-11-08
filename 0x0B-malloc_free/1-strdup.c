5733d5bdfdf12e90505da9d87e5a0d6f3b3ab902
shadowstiles <successforemmy@gmail.com>
2022-11-08 13:55:23 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - new alocated spcae in memory
+ * @str: syrong
+ * Return: pointer or NULL
+ */
+
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
