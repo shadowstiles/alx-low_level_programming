9a348d6fc7f864c8eebe40c586b82ab059d0f2fc
shadowstiles <successforemmy@gmail.com>
2022-11-08 13:43:32 -0800

    crrate array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,11 @@ char *create_array(unsigned int size, char c)
 
 	ar = malloc(size * sizeof(char));
 
+	if (ar == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 
-error:
 	free (ar);
 	return (ar);
 }
