d9f6de6a6d155f426c4fa2cf3efcb054503c8c24
shadowstiles <successforemmy@gmail.com>
2022-11-14 15:40:35 -0800

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initializing Structure
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ * @d: new poiner
+ * Return: d
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(dog));
+
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - Defining Structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: Longer Description
+ */
 
 struct dog
 {
@@ -7,5 +16,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
