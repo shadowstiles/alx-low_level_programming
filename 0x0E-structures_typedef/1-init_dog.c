5bc990fdc7aa12383c74bfcebfbd7d2d4955ef14
shadowstiles <successforemmy@gmail.com>
2022-11-14 15:58:46 -0800

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 #include "main.h"
 
 /**
@@ -12,14 +14,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(dog));
-
-	if (d == NULL)
-		return (NULL);
+	d = malloc(sizeof(struct dog));
 
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
-
-	return (d);
 }
