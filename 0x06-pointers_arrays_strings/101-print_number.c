7dded3923e84a17eacfd97675d2da027fe0f49c4
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-11-19 07:56:54 -0800

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Print an integer
+ * @n: integer to print
+ */
+
+void print_number(int n)
+{
+	int power, count, final;
+
+	count = 0;
+	power = 1;
+	final = n;
+  
+	if (n < 0)
+	{
+		_putchar('-');
+		count = 1;
+	}
+
+	while (final > 9 || final < -9)
+	{
+		power *= 10;
+		final /= 10;
+	}
+
+	while (power > 0)
+	{
+		if (power > 9)
+		{
+			if (!count)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+
+			power /= 10;
+		}
+		if (power == 1)
+		{
+			if (count)
+				_putchar((n % 10) * -1 + '0');
+			else
+				_putchar(n % 10 + '0');
+			power = 0;
+		}
+	}
+}
