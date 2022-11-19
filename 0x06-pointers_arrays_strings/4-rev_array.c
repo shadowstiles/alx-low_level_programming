19cc5e071c5b674bf6704dea6fa341508d96a55e
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-11-19 07:48:49 -0800

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,16 +10,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int i = n - 1;
+	int i;
+	int h;
 
-	while (i >= 0)
+	for (i = 0; i < (n / 2); i++)
 	{
-		if (i != (n - 1))
-		{
-			printf(", ");
-		}
-
-		printf("%d", a[i]);
-		i--;
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
 	}
 }
