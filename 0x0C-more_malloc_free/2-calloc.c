b49b405cade172e7fba867e8a19d372287db6231
shadowstiles <successforemmy@gmail.com>
2022-11-20 00:20:42 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-	p = malloc(sizeof(nmemb) * sizeof(size));
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
