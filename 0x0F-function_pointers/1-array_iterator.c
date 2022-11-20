d6e6378f98c66be9e85ed42bc48f137ea8596ae9
shadowstiles <successforemmy@gmail.com>
2022-11-20 00:51:28 -0800

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,12 +6,19 @@
  * array_iterator - Array iterator
  * @size:nsize of the array
  * @action: function pointer
+ * @array: array
  * Return: Null
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (action == NULL)
+		return;
+
+	if (array == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
