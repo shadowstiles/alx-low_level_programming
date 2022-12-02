38be290813d96f6e7cc85629f3d8e816ea1d9a4c
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-12-01 15:55:18 -1200

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * set_bit - sets value of a bit to 1 at a given index
@@ -6,6 +6,7 @@
  * @index: index position to change, starting from 0
  * Return: 1 if it worked, -1 if error
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int p;
