bb35a3669a23c4974f6e60b5380e4e1099587bce
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-12-01 15:51:28 -1200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -6,6 +6,7 @@
  * @b: char string
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int total, power;
