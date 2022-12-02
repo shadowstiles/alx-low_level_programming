dd8752c69e07c1898408fd54fa11abefe42153a3
Mbah Emmanuel <105330079+shadowstiles@users.noreply.github.com>
2022-12-01 15:56:18 -1200

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
  * clear_bit - set the value of a bit to 0 at a given index
  * @n: pointer to decimal number to change
  * @index: index position to change
  * Return: 1 if it worked, -1 if error
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
